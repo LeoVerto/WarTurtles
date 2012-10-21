b69c34f384cca656709620c2087c7c28278f0a7c
Leo Verto <leotheverto@googlemail.com>
2012-10-21 17:59:36 +0200

    Added item textures

--- MCP/src/common/leoverto/warturtles/CommonProxy.java
+++ MCP/src/common/leoverto/warturtles/CommonProxy.java
@@ -1,7 +1,7 @@
 package leoverto.warturtles;
 
 public class CommonProxy {
-	public static String ITEMS_PNG = "/leovert/warturtles/items.png";
+	public static String ITEMS_PNG = "/leoverto/warturtles/items.png";
 	public static String BLOCK_PNG = "/leoverto/warturtles/block.png";
 	
 	// Client stuff
